71a80e51343f1ee4c2d3e56a719235761721843d
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-24 17:47:09 +0100

    ✅ update graphql service

--- lib/services/graphqlService.dart
+++ lib/services/graphqlService.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:farm_sanctuary_delivery/graphql/graphql_config.dart';
+import 'package:farm_sanctuary_delivery/models/LoginDriverResponse.dart';
 import 'package:farm_sanctuary_delivery/services/sessionService.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
@@ -6,24 +9,34 @@ class GraphQLService {
   static GraphQLConfig graphQLConfig = GraphQLConfig();
   GraphQLClient client = graphQLConfig.clientToQuery();
   late final SessionService _session;
+
+  GraphQLService() {
+    _session = SessionService();
+    _session.init();
+  }
+
   Future<bool> login({
-    required String email,
+    required String login,
     required String password,
   }) async {
     try {
       QueryResult result = await client.mutate(
         MutationOptions(
-          fetchPolicy: FetchPolicy.noCache,
           document: gql("""
               mutation LoginDriver(\$input: loginDriverInput!) {
-                loginDriver(input: \$input){
-
+                loginDriver(input: \$input) {
+                      userfound
+                      passwordIsValid
+                      message
+                      agent {
+                        fullName
+                      }
+                  }
                 }
-              }
             """),
           variables: {
-            "loginDriverInput": {
-              "login": email,
+            "input": {
+              "login": login,
               "password": password,
             }
           },
@@ -33,11 +46,35 @@ class GraphQLService {
       if (result.hasException) {
         throw Exception(result.exception);
       } else {
-        var data = result.data?['loginDriver'];
-        _session.login(data.fullname, email);
-        return true;
+        print("response" + result.data.toString());
+
+        // final loginDriverResponse = LoginDriverResponse.fromJson(result.data?['loginDriver']);
+        var passwordIsValid = result.data?['loginDriver']['passwordIsValid'] as bool;
+        print("passwordIsValid" + passwordIsValid.toString());
+        if (!passwordIsValid) {
+          return false;
+        }
+        var userfound = result.data?['loginDriver']['userfound'] as bool;
+        print("userfound" + userfound.toString());
+        if (!userfound) {
+          return false;
+        }
+        var agent = result.data?['loginDriver']['agent'];
+        print("agent " + agent.toString());
+        if (agent != null) {
+          var agentfullName = agent['fullName'];
+
+          print('passwordIsValid : ' + passwordIsValid.toString());
+          print('userfound : ' + userfound.toString());
+          print('agentfullName : ' + agentfullName.toString());
+          _session.signin(agentfullName, login);
+          return true;
+        } else {
+          return false;
+        }
       }
     } catch (error) {
+      print(" error : " + error.toString());
       return false;
     }
   }

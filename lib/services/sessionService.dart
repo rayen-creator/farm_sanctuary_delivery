3ee6c93c1366b740faa9c42f2620364dc7c4a95e
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-24 17:46:13 +0100

    🐛 fix late initialize error

--- lib/services/sessionService.dart
+++ lib/services/sessionService.dart
@@ -3,36 +3,36 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SessionService {
   late SharedPreferences prefs;
 
-  late String _username;
-  late String _email;
+  String _agentfullName = "";
+  String _login = "";
   bool _loggedIn = false;
 
-  String get username => _username;
-  String get email => _email;
+  String? get agentfullName => _agentfullName;
+  String? get login => _login;
   bool get loggedIn => _loggedIn;
 
   Future<void> init() async {
     prefs = await SharedPreferences.getInstance();
-    _username = prefs.getString('username') ?? '';
-    _email = prefs.getString('email') ?? '';
+    _agentfullName = prefs.getString('agentfullName') ?? '';
+    _login = prefs.getString('login') ?? '';
     _loggedIn = prefs.getBool('loggedIn') ?? false;
   }
 
-  Future<void> login(String username, String email) async {
-    _username = username;
-    _email = email;
+  Future<void> signin(String agentfullName, String login) async {
+    _agentfullName = agentfullName;
+    _login = login;
     _loggedIn = true;
-    await prefs.setString('username', _username);
-    await prefs.setString('email', _email);
+    await prefs.setString('agentfullName', _agentfullName);
+    await prefs.setString('login', _login);
     await prefs.setBool('loggedIn', _loggedIn);
   }
 
-  Future<void> logout() async {
-    _username = '';
-    _email = '';
+  Future<void> signout() async {
+    _agentfullName = '';
+    _login = '';
     _loggedIn = false;
-    await prefs.setString('username', _username);
-    await prefs.setString('email', _email);
+    await prefs.setString('agentfullName', _agentfullName!);
+    await prefs.setString('login', _login!);
     await prefs.setBool('loggedIn', _loggedIn);
   }
 }

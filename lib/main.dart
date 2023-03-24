8190d1c2a4eac5cf7642794f6ec19af3f314a109
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-24 04:58:27 +0100

    ✅ update app name

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Farm Sanctuary Delivery',
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),

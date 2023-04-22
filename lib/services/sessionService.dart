26524db169a3b6eb56ec4382b7c653abdbe36eea
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-04-22 18:02:59 +0100

    ✅ update session service

--- lib/services/sessionService.dart
+++ lib/services/sessionService.dart
@@ -3,36 +3,36 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SessionService {
   late SharedPreferences prefs;
 
-  String _agentfullName = "";
+  String _id = "";
   String _login = "";
   bool _loggedIn = false;
 
-  String? get agentfullName => _agentfullName;
+  String? get id => _id;
   String? get login => _login;
   bool get loggedIn => _loggedIn;
 
   Future<void> init() async {
     prefs = await SharedPreferences.getInstance();
-    _agentfullName = prefs.getString('agentfullName') ?? '';
+    _id = prefs.getString('id') ?? '';
     _login = prefs.getString('login') ?? '';
     _loggedIn = prefs.getBool('loggedIn') ?? false;
   }
 
-  Future<void> signin(String agentfullName, String login) async {
-    _agentfullName = agentfullName;
+  Future<void> signin(String id, String login) async {
+    _id = id;
     _login = login;
     _loggedIn = true;
-    await prefs.setString('agentfullName', _agentfullName);
+    await prefs.setString('id', _id);
     await prefs.setString('login', _login);
     await prefs.setBool('loggedIn', _loggedIn);
   }
 
   Future<void> signout() async {
-    _agentfullName = '';
+    _id = '';
     _login = '';
     _loggedIn = false;
-    await prefs.setString('agentfullName', _agentfullName!);
-    await prefs.setString('login', _login!);
+    await prefs.setString('id', _id);
+    await prefs.setString('login', _login);
     await prefs.setBool('loggedIn', _loggedIn);
   }
 }

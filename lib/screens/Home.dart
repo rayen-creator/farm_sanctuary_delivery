78e9a94fafc490b9dfb65f5bdf248f522fa8edf1
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-05-12 12:18:29 +0100

    💄 Update UI

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -12,15 +12,20 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late Future<List<dynamic>> _deliveryFuture;
   final GraphQLService _graphQLService = GraphQLService();
-
-  final List<String> entries = <String>['A', 'B', 'C', 'D', 'E', 'F'];
-  final List<int> colorCodes = <int>[600, 500, 100];
   List<dynamic> orders = [];
 
   @override
   void initState() {
     super.initState();
+    _deliveryFuture = _graphQLService.getAlldeliveries(widget.id);
+  }
+
+  Future<void> _refreshDeliveryData() async {
+    setState(() {
+      _deliveryFuture = _graphQLService.getAlldeliveries(widget.id);
+    });
   }
 
   @override
@@ -50,94 +55,133 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: FutureBuilder<List<dynamic>>(
-        future: _graphQLService.getAlldeliveries(widget.id),
+        future: _deliveryFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
             List<dynamic> orders = snapshot.data!;
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 40, right: 40),
-                    child: const Text(
-                      "Your deliveries has been set ,\n Drive safe and have a productive day ",
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            if (orders.length > 0) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 10,
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Expanded(
-                    child: ListView.builder(
-                      physics: const ClampingScrollPhysics(),
-                      shrinkWrap: false,
-                      itemCount: orders.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                side: const BorderSide(color: Colors.blueAccent, width: 1),
-                              ),
-                              child: Column(
-                                children: [
-                                  ExpansionTile(
-                                    initiallyExpanded: true,
-                                    title: Text(
-                                      "Delivery ID : " + orders[index]['id'],
-                                      style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
-                                    ),
-                                    backgroundColor: Colors.white12,
-                                    children: [
-                                      ConstrainedBox(
-                                        constraints: const BoxConstraints(),
-                                        child: Container(
-                                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                                          child: Column(
-                                            mainAxisAlignment: MainAxisAlignment.start,
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              Text("Destination: " +
-                                                  orders[index]['location']['houseStreetnumber'] +
-                                                  ', ' +
-                                                  orders[index]['location']['city'] +
-                                                  ', ' +
-                                                  orders[index]['location']['state'] +
-                                                  ', ' +
-                                                  orders[index]['location']['country']),
-                                              // Text("Pick uplocation : " + orders[index]['location']['codePostal']),
-                                              Text("Pick uplocation : " + "Tunis"),
-                                              const SizedBox(
-                                                height: 10,
-                                              ),
-                                              ElevatedButton(
-                                                onPressed: () {},
-                                                child: Text('Delivered'),
-                                              ),
-                                              const SizedBox(
-                                                height: 10,
-                                              ),
-                                            ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40, right: 40),
+                      child: const Text(
+                        "Your deliveries has been set ,\n Drive safe and have a productive day ",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: ListView.builder(
+                        physics: const ClampingScrollPhysics(),
+                        shrinkWrap: false,
+                        itemCount: orders.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  side: const BorderSide(color: Colors.blueAccent, width: 1),
+                                ),
+                                child: Column(
+                                  children: [
+                                    ExpansionTile(
+                                      initiallyExpanded: true,
+                                      title: Text(
+                                        "Delivery ID : " + orders[index]['id'],
+                                        style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                                      ),
+                                      backgroundColor: Colors.white12,
+                                      children: [
+                                        ConstrainedBox(
+                                          constraints: const BoxConstraints(),
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text("Destination: " +
+                                                    orders[index]['location']['houseStreetnumber'] +
+                                                    ', ' +
+                                                    orders[index]['location']['city'] +
+                                                    ', ' +
+                                                    orders[index]['location']['state'] +
+                                                    ', ' +
+                                                    orders[index]['location']['country']),
+                                                // Text("Pick uplocation : " + orders[index]['location']['codePostal']),
+                                                Text("Pick uplocation : " + "Tunis"),
+                                                const SizedBox(
+                                                  height: 10,
+                                                ),
+                                                ElevatedButton(
+                                                  onPressed: () async {
+                                                    // if (_deliveryFuture != null) {
+                                                    //   // check if the field is null before accessing it
+                                                    //   await _graphQLService.updateOrderDeliveryStatus(
+                                                    //       orders[index]['id'], true);
+                                                    //   setState(() {
+                                                    //     _deliveryFuture = _graphQLService
+                                                    //         .getAlldeliveries(widget.id); // update the future
+                                                    //   });
+                                                    // }
+                                                    // print("pressed !");
+                                                    await _graphQLService.updateOrderDeliveryStatus(
+                                                        orders[index]['id'], true);
+                                                    await _refreshDeliveryData();
+                                                    setState(() {});
+                                                  },
+                                                  child: Text('Delivered'),
+                                                ),
+                                                const SizedBox(
+                                                  height: 10,
+                                                ),
+                                              ],
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            );
+                  ],
+                ),
+              );
+            } else {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.warning,
+                      color: Colors.grey[700],
+                      size: 50,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "No orders found to deliver",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
           } else {
             return const Center(child: CircularProgressIndicator());
           }
